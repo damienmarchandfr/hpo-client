f384e7ffc0cb03510d9d7e213cd8c35e4b121faa
damienmarchandfr <damien-marchand@outlook.fr>
2021-07-07 15:23:52 +0200

    example

--- src/example.ts
+++ src/example.ts
@@ -3,4 +3,13 @@ import { HPOClient } from '.'
 const client = new HPOClient()
 
 const list = client.getHPOList()
-console.log(list)
+// console.log(list)
+
+client
+	.search('a', 'terms', { max: 20, page: 1 }, false)
+	.then((r) => {
+		console.log(r)
+	})
+	.catch((err) => {
+		console.error(err)
+	})
